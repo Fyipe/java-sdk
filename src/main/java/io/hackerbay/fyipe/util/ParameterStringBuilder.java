e6e3b263531c2f97ed9ce80163deba231b3b5026
Gitlab Runner <gitlab-runner@gitlab-shell-runner-02.com>
2021-04-08 14:00:41 +0000

    staging deployment version 3.0.9872-SNAPSHOT

--- src/main/java/io/hackerbay/fyipe/util/ParameterStringBuilder.java
+++ src/main/java/io/hackerbay/fyipe/util/ParameterStringBuilder.java
@@ -1,17 +1,12 @@
 package io.hackerbay.fyipe.util;
 
 import com.google.gson.Gson;
-import io.hackerbay.fyipe.model.ErrorEvent;
-import io.hackerbay.fyipe.model.Log;
+import io.hackerbay.fyipe.model.StringLog;
 
 public class ParameterStringBuilder {
     public static String getRequestString(String applicationLogKey, String content, String logType, String [] tags) {
-        Log log = new Log(applicationLogKey, logType, content, tags);
+        StringLog log = new StringLog(applicationLogKey, logType, content, tags);
         String jsonBody = new Gson().toJson(log);
-        return jsonBody;
-    }
-    public static String getErrorEventRequestString(ErrorEvent errorEvent) {
-        String jsonBody = new Gson().toJson(errorEvent);
         return jsonBody;
     }
 }
